193bd3a55d54eae135bf7261a31a783f589a0f47
Stephen Muchendu <muchendumaina66@gmail.com>
2023-02-23 14:53:19 +0300

    starter files

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+import StateProvider from './context';
+
+import './index.scss';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <StateProvider>
+      <App />
+    </StateProvider>
+  </React.StrictMode>
+);
