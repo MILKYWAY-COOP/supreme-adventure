40bc81505022079dd339ed2f0fcbfd423cad5007
Stephen Muchendu <muchendumaina66@gmail.com>
2023-02-23 14:54:04 +0300

    starter files

---
+++ src/types/index.ts
@@ -0,0 +1,52 @@
+export interface IBook {
+  key?: string;
+  type?: string;
+  seed?: string[];
+  title: string;
+  title_suggest: string;
+  edition_count: number;
+  edition_key?: string[];
+  publish_date?: string[];
+  publish_year?: number[];
+  first_publish_year?: number;
+  number_of_pages_median?: number;
+  isbn?: string[];
+  last_modified_i: number;
+  ebook_count_i: number;
+  ebook_access: string;
+  has_fulltext: boolean;
+  public_scan_b: boolean;
+  publisher?: string[];
+  language?: string[];
+  author_key?: string[];
+  author_name?: string[];
+  publisher_facet?: string[];
+  _version_: number;
+  author_facet?: string[];
+}
+
+export interface BookWithRelevance extends IBook {
+  relevance: number;
+}
+
+export interface ISolution {
+  books: IBook[];
+  setBooks: (books: BookWithRelevance[]) => void;
+  fetchBooks: (newSearchTerm: string) => Promise<void>;
+  sortByYear: (books: IBook[]) => void;
+}
+
+export interface IBookProps {
+  title: string | undefined;
+  author: string | undefined;
+  year: number | undefined;
+  pages: number | undefined;
+  isbn?: string | undefined;
+}
+
+export interface ISearchProps {
+  fetchBooks: (newSearchTerm: string) => Promise<void>;
+  books: IBook[];
+  setBooks: (books: BookWithRelevance[]) => void;
+  sortByYear: (books: IBook[]) => void;
+}
